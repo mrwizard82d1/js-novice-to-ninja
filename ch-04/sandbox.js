57570f4f33b2ed88d05865a7d710d5320a88e341
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 19:22:32 -0600

    Copy structure from ch-02 (clean sandbox.js)

---
+++ ch-04/sandbox.js
@@ -0,0 +1,16 @@
+// Introducing camel case to name variables with multiple components
+let myAge = 25;
+let year = 2019;
+
+// Example of logging things to the console
+console.log(myAge, year);
+
+/* age = 30;
+console.log(myAge);
+
+const points = 100;
+console.log(points);
+
+// Older way to create variables
+var score = 75;
+console.log(score); */
