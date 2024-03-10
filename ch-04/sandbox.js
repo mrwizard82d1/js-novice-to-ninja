eaa3b4604b3ce7c59632d33a70ba68d6fae10e68
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 19:25:41 -0600

    Define a function (but not called!)

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,16 +1,6 @@
-// Introducing camel case to name variables with multiple components
-let myAge = 25;
-let year = 2019;
+// function declarataions
+function greet() {
+    console.log("Hi!");
+}
 
-// Example of logging things to the console
-console.log(myAge, year);
-
-/* age = 30;
-console.log(myAge);
-
-const points = 100;
-console.log(points);
-
-// Older way to create variables
-var score = 75;
-console.log(score); */
+// function expressions
