332f5ed034649d1ccb1380b3e287c8f2028bc73c
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 20:17:17 -0600

    Default values for function arguments

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,9 +1,9 @@
 // Arguments and parameters
 
 // Defines a function **argument** for the function
-const speak = function(name, time) {
+const speak = function(name = 'luigi', time = 'evening') {
     console.log(`good ${time}, ${name}`);
 }
 
 // Passes a function **parameter** into the function
-speak('mario', 'morning');
+speak();
