52d7f4bafc027d9d2d35a5a80339efba55b7896a
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 19:38:07 -0600

    Function expressions are **not** hoisted
    They must be encountered by the interpreter **before** they are called
    (used).

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,17 +1,12 @@
-// Function expressions
-const speak = function() {
-    console.log("Woof!");
-}
-
 // Calling (declared) functions
-greet();
-greet();
-greet();
+// greet();
+// greet();
+// greet();
 
 // Calling function expressions. (Yes. No difference.)
-// speak();
-// speak();
-// speak();
+speak();
+speak();
+speak();
 
 // Function declarataions
 // An example of function hoisting. Even though we define `greet` **after** we use it, the interpreter 
@@ -19,4 +14,10 @@ greet();
 // any code is executed.
 function greet() {
     console.log("hello there");
+}
+
+// Function expressions
+// However, function expressions are **not** hoisted; they must be defined before they are used.
+const speak = function() {
+    console.log("Woof!");
 }
