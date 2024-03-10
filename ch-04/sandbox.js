3a64ceaf95a16efb6c9e5d410eb0e6f9c6fc3758
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 20:19:37 -0600

    "Assignment" of default function arguments

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,9 +1,11 @@
 // Arguments and parameters
 
 // Defines a function **argument** for the function
-const speak = function(name = 'luigi', time = 'evening') {
+const speak = function(name = 'luigi', time = 'night') {
     console.log(`good ${time}, ${name}`);
 }
 
 // Passes a function **parameter** into the function
+speak();
 speak('mario');
+speak('shaun', 'day')
