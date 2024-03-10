6a58fabec07545507248941856e3ac28a6a5ddf0
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 19:35:57 -0600

    Function declarations "hoisted" before other code

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,19 +1,22 @@
-// Function declarataions
-function greet() {
-    console.log("hello there");
-}
-
 // Function expressions
 const speak = function() {
     console.log("Woof!");
 }
 
 // Calling (declared) functions
-// greet();
-// greet();
-// greet();
+greet();
+greet();
+greet();
 
 // Calling function expressions. (Yes. No difference.)
-speak();
-speak();
-speak();
+// speak();
+// speak();
+// speak();
+
+// Function declarataions
+// An example of function hoisting. Even though we define `greet` **after** we use it, the interpreter 
+// effectively moves the declaration to the "top" of the file so that it is actually defined before 
+// any code is executed.
+function greet() {
+    console.log("hello there");
+}
