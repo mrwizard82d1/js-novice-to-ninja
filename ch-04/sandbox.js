29e763f9486e0533fe7194cd4add6a9bd65a4b6a
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 19:41:52 -0600

    Clean up (everything works again)

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,23 +1,18 @@
-// Calling (declared) functions
-// greet();
-// greet();
-// greet();
-
-// Calling function expressions. (Yes. No difference.)
-speak();
-speak();
-speak();
-
 // Function declarataions
-// An example of function hoisting. Even though we define `greet` **after** we use it, the interpreter 
-// effectively moves the declaration to the "top" of the file so that it is actually defined before 
-// any code is executed.
 function greet() {
     console.log("hello there");
 }
 
 // Function expressions
-// However, function expressions are **not** hoisted; they must be defined before they are used.
 const speak = function() {
     console.log("Woof!");
 }
+// Calling (declared) functions
+greet();
+greet();
+greet();
+
+// Calling function expressions. (Yes. No difference.)
+speak();
+speak();
+speak();
