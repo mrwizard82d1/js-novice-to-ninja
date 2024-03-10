fcb9419c57cb3822748404f6dee9a668e3800eac
Larry Jones <mrwizard82d1@gmail.com>
2024-03-09 20:14:14 -0600

    Function w/ one argument; call w/ one parameter

--- ch-04/sandbox.js
+++ ch-04/sandbox.js
@@ -1,7 +1,9 @@
 // Arguments and parameters
 
+// Defines a function **argument** for the function
 const speak = function(name) {
-    console.log(`good day ${name}`);
+    console.log(`good day, ${name}`);
 }
 
-speak();
+// Passes a function **parameter** into the function
+speak('mario');
